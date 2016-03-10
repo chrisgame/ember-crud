cab23e300011e6d5817044913ee1cff8e1f4f1e4
The Game <chris.game@robbiebobbins.com>
2016-03-10 17:35:32 +0000

    updated mirage - user relationship not correct

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -18,50 +18,12 @@ export default function() {
             data: db.repositories.where({ organization: attrs.id }).map(repo => ({
               type: 'repository',
               id: repo.id
-            }))
-          }
-        }
-      })),
-    };
-  }),
-
-  this.get('/organizations/:id', function(db, request) {
-    const id = request.params.id;
-    return {
-      data: {
-        type: 'organizations',
-        id: id,
-        attributes: db.organizations.find(id),
-        relationships: {
-          repositories: {
-            links: {
-              self: `http://something.com/organizations/${id}/relationships/repositories`,
-              related: `http://something.com/organizations/${id}/repositories`
-            },
-            data: db.repositories.where({ organization: id }).map(repo => ({
-              type: 'repository',
-              id: repo.id
-            }))
-          }
-        }
-      }
-    };
-  }),
-
-  this.get('/repositories', function(db) {
-    return {
-      data: db.repositories.map(attrs => ({
-        type: 'repositories',
-        id: attrs.id,
-        attributes: attrs,
-        links: {
-          self: `http://something.com/repositories/${attrs.id}`
-        },
-        relationships: {
+            })),
+          },
           users: {
             links: {
-              self: `http://something.com/repositories/${attrs.id}/relationships/users`,
-              related: `http://something.com/repositories/${attrs.id}/users`
+              self: `http://something.com/organizations/${attrs.id}/relationships/users`,
+              related: `http://something.com/organizations/${attrs.id}/repositories/${attrs.id}/users`
             },
             data: db.users.where({ repository: attrs.id }).map(user => ({
               type: 'user',
@@ -70,49 +32,46 @@ export default function() {
           }
         }
       })),
-    };
-  }),
-
-  this.get('/repositories/:id', function(db, request) {
-    const id = request.params.id;
-    return {
-      data: {
-        type: 'repositories',
-        id: id,
-        attributes: db.repositories.find(id),
-        relationships: {
-          users: {
-            data: db.users.where({ repository: id }).map(user => ({
-              type: 'user',
-              id: user.id
-            }))
+      included: [
+        ...db.repositories.map(attrs => ({
+          type: 'repository',
+          id: attrs.id,
+          attributes: attrs,
+          links: {
+            self: `http://something.com/repositories/${attrs.id}`
+          },
+          relationships: {
+            repositories: {
+              links: {
+                self: `http://something.com/organizations/${attrs.id}/relationships/repositories`,
+                related: `http://something.com/organizations/${attrs.id}/repositories`
+              },
+              data: db.repositories.where({ organization: attrs.id }).map(repo => ({
+                type: 'repository',
+                id: repo.id
+              })),
+            },
+            users: {
+              links: {
+                self: `http://something.com/users/${attrs.id}`,
+                related: `http://something.com/organizations/${attrs.id}/repositories/${attrs.id}/users`
+              },
+              data: db.users.where({ repository: attrs.id }).map(user => ({
+                type: 'user',
+                id: user.id
+              }))
+            }
           }
-        }
-      }
-    };
-  }),
-
-  this.get('/users', function(db) {
-    return {
-      data: db.users.map(attrs => ({
-        type: 'users',
-        id: attrs.id,
-        attributes: attrs,
-        links: {
-          self: `http://something.com/users/${attrs.id}`
-        }
-      })),
-    };
-  }),
-
-  this.get('/users/:id', function(db, request) {
-    const id = request.params.id;
-    return {
-      data: {
-        type: 'users',
-        id: id,
-        attributes: db.users.find(id)
-      }
+        })),
+        ...db.users.map(attrs => ({
+          type: 'user',
+          id: attrs.id,
+          attributes: attrs,
+          links: {
+            self: `http://something.com/user/${attrs.id}`
+          }
+        }))
+      ]
     };
   })
 }
